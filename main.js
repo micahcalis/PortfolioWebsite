98f0b442f2ca190fa0e9cca0ca3484f2da70b3d1
micahcalis <micahcalis2015@gmail.com>
2024-06-01 20:49:27 +0200

    doet ie t nu?

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three/build/three.module.js';
+import * as THREE from './node_modules/three/build/three.module.js';
 import { RayMarch } from './RayMarch.js';
 import { FullscreenUnlit } from './FullscreenUnlit.js';
 import { Timer } from 'three/addons/misc/Timer.js';
