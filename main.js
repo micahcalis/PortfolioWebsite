baf85a1db2cae4f6ef400bc9a25b00f30a73c2b8
micahcalis <micahcalis2015@gmail.com>
2024-06-01 17:49:05 +0200

    ding

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as THREE from 'https://unpkg.com/three@0.124.0/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
 import { RayMarch } from './RayMarch.js';
 import { FullscreenUnlit } from './FullscreenUnlit.js';
-import { Timer } from 'https://unpkg.com/three@0.124.0/build/three.module.js/addons/misc/Timer.js';
+import { Timer } from 'https://unpkg.com/three@0.165.0/build/three.module.js/addons/misc/Timer.js';
 
 // set scenes and camera
 const fov = 75;
