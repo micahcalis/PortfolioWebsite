ad2f03b40856d0deb393f3dff3d819228705e26c
micahcalis <micahcalis2015@gmail.com>
2024-06-01 17:38:21 +0200

    bugfix 8?

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.124.0/build/three.module.js';
 import { RayMarch } from './RayMarch.js';
 import { FullscreenUnlit } from './FullscreenUnlit.js';
 import { Timer } from 'three/addons/misc/Timer.js';
